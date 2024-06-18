a2eed19dd961056f2266a83c7c2bf7ee450f2690
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-18 20:00:42 +0200

    .

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,16 @@
-var x = 1
+import { MYSQL_HOST } from '$env/static/private';
+import { MYSQL_PASS } from '$env/static/private';
+import Db from 'mysql2-async'
+const db = new Db({
+    host: MYSQL_HOST,
+    user: 'avnadmin',
+    port: 19701,
+    password: MYSQL_PASS,
+    database: 'docstore'
+})
 export async function load() {
-	return { x: x++ }
+    var c = await db.getrow("SELECT value FROM variables WHERE name='counter'")
+    var n = Number(c.value)
+    const rowsAffected = await db.update("UPDATE variables SET value = ? WHERE name='counter'", [n + 1])
+	return { x: n + 1 }
 }
