d656c6d4b8d8387e7436ef98440ed12f849bb716
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-18 08:48:39 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
     export let data
-    console.log(data)
 </script>
 <h1>SOB FullStack Példaoldal</h1>
 
-counter: {data.x}
+<div>Counter: <span>{data.x}</span></div>
+
 
 <p>
-    <a href="https://svelte.dev">Svelte</a> + <a href="https://kit.svelte.dev">SvelteKit</a> + <a
-        href="https://vitejs.dev">Vite</a>
+    <a target="_blank" href="https://svelte.dev">Svelte</a> + 
+    <a target="_blank" href="https://kit.svelte.dev">SvelteKit</a> + 
+    <a target="_blank" href="https://vitejs.dev">Vite</a>
 </p>
 
 <style>
@@ -16,5 +17,18 @@ counter: {data.x}
         background-color: #e9e8cd;
         text-align: center;
         color: rgb(11, 55, 40);
+    }
+    div {
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 20px;
+        color: rgb(65, 48, 27);
+        padding: 50px;
+    }
+    span {
+        color: rgb(27, 65, 64);
+        font-weight: bold;
+        border-radius: 7px;
+        border: solid 1px;
+        padding: 6px;
     }
 </style>
