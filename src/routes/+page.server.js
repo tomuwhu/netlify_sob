b1d3b2131117f100d3584b2a26fa79b183965e0d
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-18 18:15:21 +0200

    .

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,7 @@
-var x = 1
+import Database from 'better-sqlite3'
+const db = new Database('mydb.db')
 export async function load() {
-    x++
-	return { x }
+    const {value} = db.prepare("SELECT value FROM variables WHERE variable = ?").get('counter')
+    db.prepare("UPDATE variables SET value = ? WHERE variable = ?").run(Number(value) + 1, 'counter')
+	return { x: Number(value) + 1 }
 }
