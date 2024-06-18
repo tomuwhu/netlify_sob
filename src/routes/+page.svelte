57b2988c5d32e6ebeccf967cde965b65709b94d8
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-18 08:30:01 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>SOB FullStack Példaoldal</h1>
+
+<p>
+    <a href="https://svelte.dev">Svelte</a> + <a href="https://kit.svelte.dev">SvelteKit</a> + <a
+        href="https://vitejs.dev">Vite</a>
+</p>
+
+<style>
+    :global(body) {
+        background-color: #e9e8cd;
+        text-align: center;
+        color: rgb(11, 55, 40);
+    }
+</style>
