bd0b735bf62847ecf8f4eb13cee92b48c6350b6e
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-19 15:33:45 +0200

    .

--- src/routes/tematikak/+page.svelte
+++ src/routes/tematikak/+page.svelte
@@ -22,8 +22,12 @@
         border: solid 1px gray;
         border-radius: 10px;
         padding: 25px;
-        background-color: rgb(193, 229, 216);
+        background-color: rgb(230, 242, 207);
         box-shadow: 1px 1px 3px inset black;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 13px;
+        text-shadow: 0px 0px 2px gray;
+        color: rgb(20, 73, 64);
     }
     .cont {
         color: rgb(36, 57, 66);
@@ -31,7 +35,7 @@
         border: solid 1px gray;
         border-radius: 17px;
         padding: 10px;
-        background-color: rgb(93, 140, 123);
+        background-color: rgb(94, 94, 67);
         box-shadow: 1px 1px 3px black;
         text-align: left;
     }
@@ -43,6 +47,37 @@
         color: rgb(8, 92, 98);
         text-shadow: 1px 1px 3px gray;
         margin-top: 0px;
+        font-size: 22px;
+    }
+    :global(em) {
+        color:rgb(93, 93, 29);
+        text-shadow: 1px 1px 2px gray;
+    }
+    :global(strong) {
+        font-weight: normal;
+        color:rgb(93, 59, 29);
+        text-shadow: 1px 1px 2px gray;
+    }
+    :global(h2) {
+        font-family: 'Courier New', Courier, monospace;
+        text-align: left;
+        padding-left: 20px;
+        color: rgb(81, 101, 119);
+        text-shadow: 1px 1px 3px gray;
+        margin-top: 0px;
+        font-size: 16px;
+    }
+    :global(p) {
+        text-align: center;
+        text-shadow: 1px 1px 3px gray;
+        font-size: 14px;
+        color: rgb(81, 101, 119);
+    }
+    :global(li p) {
+         text-shadow: 0px 0px 1px gray;
+        font-size: 12px;
+        color: rgb(110, 65, 48);
+        text-align: left;
     }
     div.tv {
         padding-top: 20px;
@@ -58,7 +93,7 @@
     :global(a) {
         all: unset;
         cursor: pointer;
-        color: gray;
+        color: rgb(110, 57, 57);
     }
     :global(a:hover) {
         text-shadow: 1px 1px 3px gray;
