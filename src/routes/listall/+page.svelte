6817892177abd606f89e30ea60289ce2f305a41e
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-22 15:07:28 +0200

    .

--- src/routes/listall/+page.svelte
+++ src/routes/listall/+page.svelte
@@ -7,7 +7,7 @@
 {#if data.user == null}
     <h3>Nincs bejelentkezve!</h3>
 {:else}
-    <h3>Bejelentkezve: {data.user.name}</h3>
+    <h3>Bejelentkezve: <i>{data.user.name}</i></h3>
     <div class="fc">
     {#each data.alluser as user}
         <div class={data?.loggedinusers.includes(user.email) ? (user.email == data.user?.email ? 's1' : 's2') : 's3'}>
@@ -17,6 +17,12 @@
     </div>
 {/if}
 <style>
+    i {
+        color: rgb(97, 62, 62);
+    }
+    h3 {
+        text-shadow: 1px 1px 3px gray;
+    }
     div.fc {
         display: flex;
         flex-direction: row;
@@ -31,19 +37,20 @@
         padding-left:30px;
         padding-right:30px;
         margin: 10px;
-        border-radius: 16px;
-        border: solid 2px green;
-        box-shadow: 1px 1px 3px black;
+        border-radius: 7px;
+        border: solid 2px rgb(107, 138, 119);
+        box-shadow: 1px 1px 3px rgb(41, 36, 36);
+        text-shadow: 1px 1px 3px gray;
         white-space: nowrap;
         
     }
     :global(div.fc div.s1) {
         background-color: rgb(251, 242, 232);
-        border:solid 2px blue;
+        border:solid 2px rgb(139, 163, 197);
     }
     :global(div.fc div.s1:hover) {
-        background-color: rgb(255, 160, 160);
-        border:solid 2px red;
+        background-color: rgb(238, 227, 227);
+        border:solid 2px rgb(128, 86, 86);
     }
     :global(div.fc div.s2) {
         background-color: rgb(215, 188, 90);
@@ -56,7 +63,7 @@
         text-shadow: 1px 1px 3px gray;
     }
     :global(div.fc div.s3) {
-        background-color: rgb(179, 191, 72);
+        background-color: rgb(209, 213, 173);
     }
     :global(div.fc div.s3:hover) {
         background-color: rgb(236, 244, 170);
