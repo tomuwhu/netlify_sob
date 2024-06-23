4427c1b861eeb0323d477bbff403e000a4830fbb
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-23 17:47:39 +0200

    .

--- src/routes/toplist/+page.svelte
+++ src/routes/toplist/+page.svelte
@@ -3,6 +3,18 @@
     import { browser } from '$app/environment'
     import { goto } from '$app/navigation'
     if (browser && data.user == null) goto('/login')
+    function f1(n, x) {
+        return Math.log(1+Number(n) * 5 - Math.round(Number(n) * Number(x)))
+    }
+    function f2 (n, x) {
+        return Math.log(1 + Number(n) * Number(x))
+    }
+    function f3 (n, x) {
+        return Number(n) * 5 - Math.round(Number(n) * Number(x))
+    }
+    function f4 (n, x) {
+        return Number(n) * Number(x)
+    }
 </script>
 <h1>Toplista</h1>
 <table>
@@ -10,8 +22,7 @@
         <th></th>
         <th>Név</th>
         <th>Hivatkozás</th>
-        <th>Átlagos értékelés</th>
-        <th>Összes értékelés</th>
+        <th colspan=2 class=the>Értékelés</th>
         <th>Pontszám</th>
     </tr>
 {#each data.toplist as row, rid}
@@ -20,8 +31,8 @@
         <td class=l><div>{row.name}.</div></td>
         <td class=l><a href={row.pfurl} class="view" target="_blank">{row.pfurl}</a></td>
         <td class=c>{Number(row.st).toFixed(2)}</td>
-        <td class=r>{#each Array(Math.round(Number(row?.n)*5 - Math.round(Number(row?.n)*Number(row?.st)))).fill() as x, i}<i>☆</i>{/each}{#each Array(Math.round(Number(row?.n)*Number(row?.st))).fill() as x, i}★{/each}</td>
-        <td class="c x">{Number(row.score).toFixed(2)}</td>
+        <td class=r>{#each Array(Math.round(f1(row?.n, row?.st))).fill() as x, i}<i>☆</i>{/each}{#each Array(Math.round(f2(row?.n, row?.st))).fill() as x, i}★{/each} <b> | <em>{f3(row?.n, row?.st)}</em> + {f4(row?.n, row?.st)} </b></td>
+        <td class="r x">{Number(row.score).toFixed(2)}</td>
     </tr>
 {/each}
 </table>
@@ -31,6 +42,12 @@
     }
     h1 {
         padding-top: 20px;
+    }
+    th.the {
+        text-align: left;
+    }
+    th.thr {
+        text-align: left;
     }
     th.rid {
         text-align: right;
@@ -58,11 +75,12 @@
     }
     td.l {
         text-align: left;
+        box-shadow: 1px 1px 3px inset rgb(200, 200, 200);
     }
     td.c {
         font-family: 'Courier New', Courier, monospace;
         text-align: center;
-        background-color: rgb(27, 81, 81);
+        background-color: rgb(35, 121, 121);
         color: rgb(235, 237, 207);
         font-weight: bold;
     }
@@ -81,11 +99,23 @@
         text-shadow: none;
         text-shadow: 0px 0px 2px rgb(137, 137, 137);
     }
+    td.r b {
+        all: unset;
+        font-size: 14px;
+        color: rgb(20, 109, 0);
+        text-shadow: none;
+    }
+    td.r em {
+        all: unset;
+        font-size: 14px;
+        color: rgb(239, 191, 0);
+        text-shadow: none;
+    }
     td a {
         all: unset;
         cursor: pointer;
         color: rgb(90, 149, 129);
-        text-shadow: 1px 1px 3px gray;
+        text-shadow: 0px 0px 2px gray;
     }
     td a:hover {
         all: unset;
@@ -94,7 +124,11 @@
         text-shadow: 1px 1px 3px gray;
     }
     td.x {
-        color: gold;
-        background-color: rgb(110, 97, 15);
+        font-size: 17px;
+        color: rgb(125, 139, 0);
+        background: none;
+        box-shadow: none;
+        text-shadow: 0px 0px 2px rgb(137, 137, 137);
+        padding: 2px;
     }
 </style>
